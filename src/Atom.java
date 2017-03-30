e02812a7e468417d6ed5202e0b323e2c6bc9e469
Siderskini <siddharth.viswanathan@gmail.com>
2017-03-29 21:05:10 -0700

    Atom class

--- src/Atom.java
+++ src/Atom.java
@@ -1,44 +1,77 @@
 /**
- * Created by Siderskini1 on 3/29/17.
+ * Holds an Atom object
  */
 public class Atom {
-    private int v, p, n, e;
+    //All atoms have valence electrons and subatomic particles
+    private int valence, protons, neutrons, electrons;
 
+    //Default Constructor
     public Atom() {
         this(1, 1);
     }
 
-    public Atom(int a, int z) {
-        this(a, z, 0);
+    //Main Constructor
+    public Atom(int atomicNum, int atomicMass) {
+        this(atomicNum, atomicMass, 0);
     }
 
-    public Atom(int a, int z, int c) {
-        p = a;
-        n = z - a;
-        e = p - c;
-        if (e < 3) {
-            v = e;
-        } else if (e < 21) {
-            v = Math.floorMod(e + 6, 8);
-        } else if (e < 57) {
-            if ((e - 3) % 18 < 10) {
-                v = -1;
+    //Constructor if there's a charge
+    public Atom(int atomicNum, int atomicMass, int charge) {
+        protons = atomicNum;
+        neutrons = atomicMass - atomicNum;
+        electrons = protons - charge;
+        if (electrons < 3) {
+            valence = electrons;
+        } else if (electrons < 21) {
+            valence = Math.floorMod(electrons + 6, 8);
+        } else if (electrons < 57) {
+            if ((electrons - 3) % 18 < 10) {
+                valence = -1;
             } else {
-                if (e % 18 < 3) {
-                    v = e % 18;
+                if (electrons % 18 < 3) {
+                    valence = electrons % 18;
                 }
                 else {
-                    v = (e + 8) % 18;
+                    valence = (electrons + 8) % 18;
                 }
             }
         }
-        if (v == 0) {
-            v = 8;
+        if (valence == 0) {
+            if (atomicNum == 1) {
+                valence = 0;
+            } else if (atomicNum < 3) {
+                valence = 2;
+            } else {
+                valence = 8;
+            }
         }
     }
 
+    //Resturns the number of valence electrons
     public int getValence() {
-        return v;
+        return valence;
+    }
+
+    //Returns the formal charge on the atom
+    public int formalCharge() {
+        return valence - loners() - numBonds();
+    }
+
+    /**
+     * Returns the total number of bonded electrons divided by 2
+     *This is equal to the number of bonds, counting double/triple bonds accordingly
+    */
+    public int numBonds() {
+        return 0;
+    }
+
+    //Returns the number of lone electrons surrounding the atom
+    public int loners() {
+        if (protons > 2) {
+            return 8 - 2 * numBonds();
+        } else {
+            return 0;
+        }
     }
 
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,3 +1,7 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Created by Siderskini1 on 3/29/17.
  */
@@ -6,9 +10,19 @@ public class Tester {
     public static void main(String[] args) {
         Atom x;
         for (int i = 1; i < 57; i++) {
-            x = new Atom(i, 100);
+            x = new Atom(i, 100, 1);
             System.out.println(x.getValence());
         }
     }
 
+    @Test
+    public void valenceTest() {
+        int[] check = {1, 2, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8, 1, 2, -1, -1, -1, -1, -1, -1, -1, -1,
+                -1, -1, 3, 4, 5, 6, 7, 8, 1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 3, 4, 5, 6, 7, 8, 1, 2};
+        Atom x;
+        for (int i = 1; i < 57; i++) {
+            x = new Atom(i, 100);
+            assertEquals(check[i - 1], x.getValence());
+        }
+    }
 }
